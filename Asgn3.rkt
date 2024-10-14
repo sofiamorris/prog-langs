23491941be5f038e4b86f11587d46f5816eb79e2
sofiamorris <sofiamorris213@gmail.com>
2024-10-14 15:09:13 -0700

    interp and parse

--- Asgn3.rkt
+++ Asgn3.rkt
@@ -9,7 +9,7 @@
 ;;represents a number
 (struct NumC([n : Real]) #:transparent)
 ;;represents types of binary operations
-(struct BinopC ([op : Symbol] [left : ArithC] [right : ArithC]))
+(struct BinopC ([op : Symbol] [left : ArithC] [right : ArithC]) #:transparent)
 
 ;;takes in an operator symbol and two arithmetic functions and returns the result of the operation
 (define (binop-interp [op : Symbol] [l : ArithC] [r : ArithC]) : Real
@@ -25,5 +25,17 @@
     [(NumC n) n]
     [(BinopC op l r) (binop-interp op l r)]))
 
-(interp (BinopC '+ (NumC 2) (NumC 2)))
+(check-equal? (interp (BinopC '+ (NumC 2) (NumC 2))) 4)
+(check-equal? (interp (BinopC '* (NumC 3) (NumC 2))) 6)
+(check-equal? (interp (BinopC '/ (NumC 2) (NumC 2))) 1)
+(check-equal? (interp (BinopC '- (NumC 2) (NumC 2))) 0)
+
 ;;parser in Arith takes in an s-expression and returns a corresponding ArithC or signals an error
+(define (parse [s : Sexp]) : ArithC
+  (match s
+    [(? real? n) (NumC n)]
+    [(list (? symbol? op) l r) (BinopC op (parse l) (parse r))]))
+
+(check-equal? (parse '7) (NumC 7))
+(check-equal? (parse '{* {+ 2 3} 7}) (BinopC '* (BinopC '+ (NumC 2) (NumC 3)) (NumC 7)))
+
