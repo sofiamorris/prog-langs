9b1dbf907c1c09dfbd2621663b1640a6de6a0466
sofiamorris <sofiamorris213@gmail.com>
2024-10-14 15:15:29 -0700

    arith to expr

--- Asgn3.rkt
+++ Asgn3.rkt
@@ -5,14 +5,14 @@
 ;;<Submission progress comment>
 
 ;;represents types of arithmetic extressions
-(define-type ArithC (U NumC BinopC))
+(define-type ExprC (U NumC BinopC))
 ;;represents a number
 (struct NumC([n : Real]) #:transparent)
 ;;represents types of binary operations
-(struct BinopC ([op : Symbol] [left : ArithC] [right : ArithC]) #:transparent)
+(struct BinopC ([op : Symbol] [left : ExprC] [right : ExprC]) #:transparent)
 
 ;;takes in an operator symbol and two arithmetic functions and returns the result of the operation
-(define (binop-interp [op : Symbol] [l : ArithC] [r : ArithC]) : Real
+(define (binop-interp [op : Symbol] [l : ExprC] [r : ExprC]) : Real
   (match op
     ['* (* (interp l) (interp r))]
     ['+ (+ (interp l) (interp r))]
@@ -20,7 +20,7 @@
     ['- (- (interp l) (interp r))]))
 
 ;;takes in an arithmetic expression and reduces it to its value
-(define (interp [exp : ArithC])
+(define (interp [exp : ExprC])
   (match exp 
     [(NumC n) n]
     [(BinopC op l r) (binop-interp op l r)]))
@@ -31,7 +31,7 @@
 (check-equal? (interp (BinopC '- (NumC 2) (NumC 2))) 0)
 
 ;;parser in Arith takes in an s-expression and returns a corresponding ArithC or signals an error
-(define (parse [s : Sexp]) : ArithC
+(define (parse [s : Sexp]) : ExprC
   (match s
     [(? real? n) (NumC n)]
     [(list (? symbol? op) l r) (BinopC op (parse l) (parse r))]))
