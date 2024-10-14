284a6813f774516689cbb8e637ee7fb12ac3c8da
sofiamorris <sofiamorris213@gmail.com>
2024-10-14 13:45:56 -0700

    restructuring

--- Asgn3.rkt
+++ Asgn3.rkt
@@ -6,29 +6,22 @@
 
 ;;represents types of arithmetic extressions
 (define-type ArithC (U NumC BinopC))
-;;represents types of binary operations
-(define-type BinopC (U PlusC MultC SubC DivC))
 ;;represents a number
 (struct NumC([n : Real]) #:transparent)
-;;represents addition
-(struct PlusC([left : ArithC] [right : ArithC]) #:transparent)
-;;represents multiplication
-(struct MultC([left : ArithC] [right : ArithC]) #:transparent)
-;;represents subtraction
-(struct SubC([left : ArithC] [right : ArithC]) #:transparent)
-;;represents division
-(struct DivC([left : ArithC] [right : ArithC]) #:transparent)
+;;represents types of binary operations
+(struct BinopC ([op : Symbol] [left : ArithC] [right : ArithC]))
 
 ;;hashtable to lookup Binary operators
 (define ht
-  (make-immutable-hash '([PlusC . '+] [MultC . '*] [SubC . '-] [DivC . '/])))
+  #hash(['+ . +] ['* . *] ['- . -] ['/ . /]))
 
 ;;takes in an arithmetic expression and reduces it to its value
-(define (interp [exp : ArithC]) : Real
+(define (interp [exp : ArithC])
   (match exp 
-    [(NumC n) n]
-    [(? (lambda (exp) (hash-has-key? ht exp)))
-     (define op
-       (hash-ref ht exp)) (op (interp ...) (interp ...))]))
+    [(NumC n) (printf "~a" n)]
+    [(BinopC op l r)
+     (define sign
+       (hash-ref ht op)) (printf "~a" sign)]))
 
+(interp (BinopC '+ (NumC 2) (NumC 2)))
 ;;parser in Arith takes in an s-expression and returns a corresponding ArithC or signals an error
