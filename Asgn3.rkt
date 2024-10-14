0c056fcca168c28c55a81c565437a1857140efc7
sofiamorris <sofiamorris213@gmail.com>
2024-10-14 14:12:50 -0700

    working 3.1

--- Asgn3.rkt
+++ Asgn3.rkt
@@ -11,17 +11,19 @@
 ;;represents types of binary operations
 (struct BinopC ([op : Symbol] [left : ArithC] [right : ArithC]))
 
-;;hashtable to lookup Binary operators
-(define ht
-  #hash(['+ . +] ['* . *] ['- . -] ['/ . /]))
+;;takes in an operator symbol and two arithmetic functions and returns the result of the operation
+(define (binop-interp [op : Symbol] [l : ArithC] [r : ArithC]) : Real
+  (match op
+    ['* (* (interp l) (interp r))]
+    ['+ (+ (interp l) (interp r))]
+    ['/ (/ (interp l) (interp r))]
+    ['- (- (interp l) (interp r))]))
 
 ;;takes in an arithmetic expression and reduces it to its value
 (define (interp [exp : ArithC])
   (match exp 
-    [(NumC n) (printf "~a" n)]
-    [(BinopC op l r)
-     (define sign
-       (hash-ref ht op)) (printf "~a" sign)]))
+    [(NumC n) n]
+    [(BinopC op l r) (binop-interp op l r)]))
 
 (interp (BinopC '+ (NumC 2) (NumC 2)))
 ;;parser in Arith takes in an s-expression and returns a corresponding ArithC or signals an error
