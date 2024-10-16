83f7718dbd798b1d17546cbbb20c23e7900d41a9
anissasoungpanya <122430923+anissasoungpanya@users.noreply.github.com>
2024-10-15 21:46:25 -0700

    Update Asgn3.rkt

--- Asgn3.rkt
+++ Asgn3.rkt
@@ -5,17 +5,15 @@
 ;;<Submission progress comment>
 
 ;;represents types of arithmetic extressions
-(define-type ExprC (U NumC BinopC ifleq0? Symbol AppC IdC))
+(define-type ExprC (U NumC BinopC Symbol AppC IdC))
 ;;represents a number
 (struct NumC([n : Real]) #:transparent)
 ;;represents types of binary operations
 (struct BinopC ([op : Symbol] [left : ExprC] [right : ExprC]) #:transparent)
-;;represents conditional <=0
-(struct ifleq0? ([a : ExprC] [b : ExprC] [c : ExprC]) #:transparent)
-;;represents function definitions
+;;represents function defnitions
 (struct FundefC ([name : Symbol] [args : (Listof Symbol)] [body : ExprC]) #:transparent)
 ;; represents application form
-(struct AppC ([fun : Symbol] [arg : Symbol]) #:transparent)
+(struct AppC ([fun : Symbol] [arg : (Listof ExprC)]) #:transparent)
 ;; represents idC
 (struct IdC ([name : Symbol]))
 
@@ -27,14 +25,16 @@
     ['/ (/ (interp l funs) (interp r funs))]
     ['- (- (interp l funs) (interp r funs))]))
 
+
 ;;takes in an arithmetic expression and reduces it to its value
-(define (interp [exp : ExprC] [funs : (Listof FundefC)]) : Real
+(define (interp [exp : ExprC][funs : (Listof FundefC)]) : Real
   (match exp 
     [(NumC n) n]
-    [(AppC fun args) (subst args (FundefC-args (find-fun fun funs)) (FundefC-body (find-fun fun funs)))]
-    [(BinopC op l r) (binop-interp op l r funs)]
-    [(ifleq0? a b c) (if (<= (interp a) (interp (NumC 0))) (interp b) (interp c))]))
-
+    [(AppC fun args) (cond
+                       [(not (= (length args) 1)) (error 'interp "AAQZ more than one arg")]
+                       [else
+                        (subst (first args) (FundefC-args (find-fun fun funs)) (FundefC-body (find-fun fun funs)))])]
+    [(BinopC op l r) (binop-interp op l r funs)]))
 
 
 ;; helper function to find function definition
@@ -43,6 +43,9 @@
     [(empty? funs) (error 'find-fun "AAQZ function not found")]
     [(equal? fun (FundefC-name (first(funs)))) (first(funs))]
     [else (find-fun (rest funs))]))
+
+(check-equal? (find-fun 'f (list (FundefC 'f '(x y) (BinopC '+ 'x 'y)))) (FundefC 'f '(x y) (BinopC '+ 'x 'y)))
+(check-exn #rx"AAQZ" (lambda () (find-fun 'hi (list (FundefC 'f '(x y) (BinopC '+ 'x 'y))))))
 
 ;; helper function to interpret single-argument applications
 (define (subst [what : ExprC][for : Symbol][in : ExprC]) : ExprC
@@ -56,12 +59,10 @@
                       (subst what for l)
                       (subst what for r))]))
 
-(check-equal? (interp (BinopC '+ (NumC 2) (NumC 2))) 4)
-(check-equal? (interp (BinopC '* (NumC 3) (NumC 2))) 6)
-(check-equal? (interp (BinopC '/ (NumC 2) (NumC 2))) 1)
-(check-equal? (interp (BinopC '- (NumC 2) (NumC 2))) 0)
-(check-equal? (interp (ifleq0? (NumC 5) (NumC 1) (NumC 0))) 0)
-(check-equal? (interp (ifleq0? (NumC -1) (NumC 1) (NumC 0))) 1)
+(check-equal? (interp (BinopC '+ (NumC 2) (NumC 2)) '()) 4)
+(check-equal? (interp (BinopC '* (NumC 3) (NumC 2)) '()) 6)
+(check-equal? (interp (BinopC '/ (NumC 2) (NumC 2)) '()) 1)
+(check-equal? (interp (BinopC '- (NumC 2) (NumC 2)) '()) 0)
 
 ;;parser in Arith takes in an s-expression and returns a corresponding ArithC or signals an error
 (define (parse [s : Sexp]) : ExprC
@@ -70,25 +71,11 @@
     [(? symbol? s) s]
     [(list (? symbol? fun) (? symbol? arg)) (AppC fun arg)]
     [(list (? symbol? op) l r) (BinopC op (parse l) (parse r))]
-    [(list 'ifleq0? a b c) (ifleq0? (parse a) (parse b) (parse c))]
     [other (error 'parse "AAQZ expected a valid Sexp, got ~e" other)]))
 
 (check-equal? (parse '7) (NumC 7))
 (check-equal? (parse '{* {+ 2 3} 7}) (BinopC '* (BinopC '+ (NumC 2) (NumC 3)) (NumC 7)))
-(check-equal? (parse '{ifleq0? 5 1 0}) (ifleq0? (NumC 5) (NumC 1) (NumC 0)))
 (check-exn #rx"AAQZ" (lambda () (parse "hi")))
-
-;; top-interp takes in s-expression and
-;; calls parse and interp, reducing the
-;; expression to a value
-(define (top-interp [s : Sexp]) : Real
-  (interp(parse s)))
-
-(check-equal? (top-interp '7) 7)
-(check-equal? (top-interp '{* {+ 2 3} 7}) 35)
-(check-equal? (top-interp '{ifleq0? 5 1 0}) 0)
-(check-exn #rx"AAQZ" (lambda () (top-interp "hi")))
-
 
 ;; parser that takes s-expression and returns FundefC's
 (define (parse-fundef [s : Sexp]) : FundefC
@@ -111,4 +98,24 @@
 (check-exn #rx"AAQZ" (lambda () (parse-fundef '{def err {(x x) => x}})))
 (check-exn #rx"AAQZ" (lambda () (parse-fundef '{def err {(x y)}})))
 
+(check-equal? (has-dup '{h h}) #t)
+(check-equal? (has-dup '{w s}) #f)
+
+
+;; parser that takes s-expression with FundefC and returns list of FundefC
+(define (parse-prog [s : Sexp]) : (Listof FundefC)
+  (match s
+    [(list (? symbol? r)...) (list (parse-fundef r) (parse-prog (rest s)))]
+    [other (error 'parse-prog "AAQZ improper syntax ~e" other)]))
+
+
+
+;; top-interp takes in s-expression and
+;; calls parse and interp, reducing the
+;; expression to a value
+#;(define (top-interp [fun-sexps : Sexp]) : Real
+  (interp-fn (parse-prog fun-sexps)))
+
+#;(check-equal? (top-interp '{{def f {(x y) => {+ x y}}}
+                     {def main {() => {f 1 2}}}}) 3)
 
